da500313e8af071d1c082743432cc30c933434ff
Yannick Warnier <ywarnier@beeznest.org>
2017-08-10 22:11:18 +0200

    Remove non-mandatory type-hinting in equals() method

--- Entity/Agent.php
+++ Entity/Agent.php
@@ -26,7 +26,7 @@ class Agent extends Actor
     /**
      * {@inheritdoc}
      */
-    public function equals(Actor $actor)
+    public function equals($actor)
     {
         if (!parent::equals($actor)) {
             return false;
